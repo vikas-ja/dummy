bc9ca23d50f6c3e3071b786ce642d149cdeed8f3
vikas.jain <vikas.jain@incture.com>
2025-03-25 14:19:18 +0530

    added: component scan properties

--- src/main/java/com/artifact_test/demo/DemoApplication.java
+++ src/main/java/com/artifact_test/demo/DemoApplication.java
@@ -5,15 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.cherrywork.oneapp.fieldcatalog.controller.LapiController;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.cherrywork.oneapp.fieldcatalog.*")
+@ComponentScan(basePackages = "com.*")
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
-
 }
